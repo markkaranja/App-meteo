78605280e61fbfc5326cb8c68a93aab1f5855712
Karanja Mark <mark.karanja@etudiant.univ-reims.frexitgit --versiongit config --global user.email mark.karanja@etudiant.univ-reims.fr>
2026-09-20 13:52:16 +0200

    Ajout de la fonction async getWeatherData

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const tempValue = document.querySelector("#temp-value");
 const humidityVal = document.querySelector("#humidity-val");
 const pressureVal = document.querySelector("#pressure-val");
 
+const apiKey = "9e0a490997384b95986bb811ed3615db";
+
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault()
 
@@ -16,6 +18,19 @@ searchForm.addEventListener("submit", (e) => {
 
     cityInput.value = "";
 
-    console.log(cityName)
+    getWeatherData(cityName);
 })
+
+async function getWeatherData(city){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=fr`
+    const reponse = await fetch(apiUrl)
+
+    if(!reponse.ok){
+        // Envoie un message d'erreur si ils ne trouve pas la ville
+        throw new Error("Ville non trouvé")
+    }
+
+    return await reponse.json();
+}
+
  
